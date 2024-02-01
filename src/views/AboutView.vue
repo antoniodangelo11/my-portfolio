3422d2035fb59130937a76ccd9fa028aab6ee2eb
Antonio D'Angelo <reapercsp@gmail.com>
2024-02-01 17:35:47 +0100

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <main>
+    <h1>SONO L'ABOUT</h1>
+  </main>
+</template>
+
+<style></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <main>
+    <h1>SONO LA HOME</h1>
+  </main>
+</template>
+
+<style></style>
