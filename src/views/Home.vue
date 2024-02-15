10d68849b5b33120e346f611858a974ae171ac61
Antonio D'Angelo <reapercsp@gmail.com>
2024-02-15 19:05:42 +0100

    update card

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
 
 <template>
   <main>
-    <h1 class="mt-10">SONO LA HOME</h1>
+    <h1 class="text-center text-red-600 font-bold text-2xl">Benvenuto nel mio Portfolio</h1>
   </main>
 </template>
 
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -7,17 +7,35 @@
             id: 1,
             title: "Social Posts",
             collaborators: "No",
-            description: "Clone of Facebook",
+            description: "Facebook clone",
             image: "./src/assets/img/socialpost.png",
-            // image2: "/01SocialPosts2.jpeg",
-            // image3: "/01SocialPosts3.jpeg",
-            // image4: "/01SocialPosts4.jpeg",
-            // image5: "/01SocialPosts5.jpeg",
             video: "",
             technologies: "HTML, CSS, JS",
-            link_netlify: "",
+            link_netlify: "https://antonio-dangelo-socialpost.netlify.app",
             link_github: "https://github.com/antoniodangelo11/js-social-posts",
-          }
+          },
+          {
+            id: 2,
+            title: "Spotify Web",
+            collaborators: "No",
+            description: "SpotifyWeb clone",
+            image: "./src/assets/img/spotifyweb.png",
+            video: "",
+            technologies: "HTML, CSS",
+            link_netlify: "https://antonio-dangelo-spotifyweb.netlify.app",
+            link_github: "https://github.com/antoniodangelo11/html-css-spotifyweb.git",
+          },
+          {
+            id: 3,
+            title: "Avada Driving",
+            collaborators: "No",
+            description: "Avada Driving clone",
+            image: "./src/assets/img/avadadriving.png",
+            video: "",
+            technologies: "HTML, CSS",
+            link_netlify: "https://antonio-dangelo-avadadriving.netlify.app",
+            link_github: "https://github.com/antoniodangelo11/proj-html-vuejs.git",
+          },
         ]
       }
     },
@@ -26,32 +44,35 @@
 
 <template>
   <main>
-    <h1 class="text-center">MY PROJECT</h1>
+    <div class="container_card m-auto">
+      <h1 class="text-center text-red-600 font-bold text-2xl mb-3">My Projects</h1>
 
-    <div class="max-w-sm bg-blue-400 border border-gray-200 rounded-lg shadow dark:bg-gray-100 dark:border-gray-700"
-      v-for="project in projects" :key="project.id"
-    >
-      <a href="#">
-        <img class="rounded-t-lg" :src="project.image" alt=""/>
-      </a>
+      <div class="flex flex-wrap justify-center gap-3">
+        <div v-for="project in projects" :key="project.id">
 
-      <div class="p-5">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ project.title }}</h5>
+          <div class="max-w-sm h-full w-80 bg-blue-300 border border-gray-200 rounded-lg shadow dark:bg-gray-100 dark:border-gray-700">
+            <a href="#">
+              <img class="rounded-t-lg h-52 w-96" :src="project.image" alt=""/>
+            </a>
 
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"><span class="font-bold">Description: </span>{{ project.description }}</p>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"><span class="font-bold">Technologies: </span>{{ project.technologies }}</p>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"><span class="font-bold">Collaborators: </span>{{ project.collaborators }}</p>
+            <div class="p-5">
+              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ project.title }}</h5>
+              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"><span class="font-bold">Description: </span>{{ project.description }}</p>
+              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"><span class="font-bold">Technologies: </span>{{ project.technologies }}</p>
+              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"><span class="font-bold">Collaborators: </span>{{ project.collaborators }}</p>
+              <a :href="project.link_netlify" class="inline-flex items-center px-3 py-2 mr-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Netlify
+              </a>
+              <a :href="project.link_github" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Github
+              </a>
+            </div>
+          </div>
 
-        <a :href="project.link_netlify" class="inline-flex items-center px-3 py-2 mr-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-          Netlify
-        </a>
-
-        <a :href="project.link_github" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-          Github
-        </a>
+        </div>
       </div>
-
     </div>
+    
   </main>
 </template>
 
